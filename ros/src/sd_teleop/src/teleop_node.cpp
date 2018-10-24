1b8c2cdc34fff124997cf11ddc2f7e50583a81cb
Julien BAYLE <jbayle@gmail.com>
2018-10-24 15:21:45 +0200

    Teleop node tested with a pad, it works

--- ros/src/sd_teleop/src/teleop_node.cpp
+++ ros/src/sd_teleop/src/teleop_node.cpp
@@ -15,6 +15,7 @@ double max_linear_x_velocity;
 double max_linear_y_velocity;
 double max_angular_z_velocity;
 double boost_velocity_ratio;
+std::string joy_topic_name;
 
 // Actual boost ratio
 double current_boost_ratio;
@@ -24,7 +25,15 @@ bool last_change_mode_state = false;
 
 // cmd_vel source index and array
 std::vector<std::string> cmd_vel_sources;
-int cmd_vel_sources_index = 0;
+unsigned int cmd_vel_sources_index = 0;
+
+
+void publish_cmd_vel_source() {
+	std_msgs::String cmd_vel_source;
+	cmd_vel_source.data = cmd_vel_sources[cmd_vel_sources_index];
+	cmd_vel_source_publisher.publish(cmd_vel_source);
+	ROS_INFO("Change cmd_vel source to %s", cmd_vel_source.data.c_str());
+}
 
 
 void joy_handler(const sensor_msgs::Joy::ConstPtr& joy_msg)
@@ -53,13 +62,10 @@ void joy_handler(const sensor_msgs::Joy::ConstPtr& joy_msg)
 
 	if (!joy_msg->buttons[XBOX_BUTTON_START] && last_change_mode_state) {
 		cmd_vel_sources_index++;
-		if(cmd_vel_sources_index = cmd_vel_sources.size()) {
+		if(cmd_vel_sources_index == cmd_vel_sources.size()) {
 			cmd_vel_sources_index = 0;
 		}
-		std_msgs::String cmd_vel_source;
-		cmd_vel_source.data = cmd_vel_sources[cmd_vel_sources_index];
-		cmd_vel_source_publisher.publish(cmd_vel_source);
-		ROS_INFO("Change teleop source to %s", cmd_vel_source.data.c_str());
+		publish_cmd_vel_source();
 	}	
 
 	last_change_mode_state = joy_msg->buttons[XBOX_BUTTON_START] > 0;
@@ -76,12 +82,17 @@ int main(int argc, char **argv)
     nh.param("max_linear_y_velocity", max_linear_y_velocity, 0.0);
     nh.param("max_angular_z_velocity", max_angular_z_velocity, 1.0);
     nh.param("boost_velocity_ratio", boost_velocity_ratio, 2.0);
-	nh.param("cmd_vel_sources", cmd_vel_sources, cmd_vel_sources);    
-
+    nh.param("cmd_vel_sources", cmd_vel_sources, cmd_vel_sources);    
+    nh.param("joy_topic", joy_topic_name, joy_topic_name);
+    
     cmd_vel_teleop_publisher = nh.advertise<geometry_msgs::Twist>("cmd_vel_teleop", 1);
     cmd_vel_source_publisher = nh.advertise<std_msgs::String>("cmd_vel_source", 1, true);
     
-    ros::Subscriber joy_subscriber = nh.subscribe<sensor_msgs::Joy>("joy", 1, joy_handler);
+    ros::Subscriber joy_subscriber = nh.subscribe<sensor_msgs::Joy>(joy_topic_name, 1, joy_handler);
     
+    // Publish initial cmd_source to init the cmd_vel_selector
+    publish_cmd_vel_source();
+    
+    // Main loop
     ros::spin();
 }
