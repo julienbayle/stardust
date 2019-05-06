d08ee5bac6edd9ae451cfff53eaf75af5c28c26b
Julien BAYLE <jbayle@gmail.com>
2019-05-06 19:06:56 +0200

    Update dependencies without build
    Usefull to update dependencies without building code 
    (cross compilation use case)

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,6 +4,24 @@ if [ -f /tmp/ros.pid ]; then
         echo "Merci d'arrêter de ROS avant de lancer une mise à jour"
 fi
 
+DO_BUILD="YES"
+
+# Parse values
+for i in "$@"
+do
+case $i in
+    --no-build)
+    DO_BUILD="NO"
+    shift # past argument=value
+    ;;
+    *)
+        echo "Usage update.sh "
+        echo "  --no-build              (do not execute catkin-make)"
+        exit 1 
+    ;;
+esac
+done
+
 PROJECTDIR=$(dirname "$0")
 echo $PROJECTDIR
 PROJECTDIR=`cd $PROJECTDIR/.. && pwd`
@@ -16,4 +34,7 @@ echo "Updating WSTOOL dependencies (.rosinstall)"
 wstool update -t src
 echo "Updating ROSDEPS (package dependencies)"
 rosdep install --from-paths src --ignore-src --rosdistro melodic -r -y
-catkin_make
+
+if [[ "$DO_BUILD" == "YES" ]]; then
+        catkin_make
+fi
