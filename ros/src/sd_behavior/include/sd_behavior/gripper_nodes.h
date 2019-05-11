f3f0cc7c630a8e3d9461dabfe9359a7435a40956
Noxaeternae <guillaume.romand@gmail.com>
2019-05-11 09:46:51 -0700

    Adding pump and valves nodes

--- ros/src/sd_behavior/include/sd_behavior/gripper_nodes.h
+++ ros/src/sd_behavior/include/sd_behavior/gripper_nodes.h
@@ -4,7 +4,7 @@
 
 namespace GripperNodes
 {
-	void registerNodes(BT::BehaviorTreeFactory& factory);
+   void registerNodes(BT::BehaviorTreeFactory& factory);
 
     class GripperNode : public BT::SyncActionNode
 	{
@@ -13,16 +13,39 @@ namespace GripperNodes
 
 			BT::NodeStatus tick() override;
 
-		    static BT::PortsList providedPorts()
-		    {
+		    	static BT::PortsList providedPorts()
+		    	{
 			    return { 
-			    	BT::InputPort<double>("angle_base"), 
+				    	BT::InputPort<bool>("left_valve"), 
+				    	BT::InputPort<bool>("middle_valve"), 
+				    	BT::InputPort<bool>("right_valve"), 
+				    	BT::InputPort<bool>("pump") 
+				};
+			}
+		private:
+			ros::Publisher activation_left_valve_pub_;
+			ros::Publisher activation_middle_valve_pub_;
+			ros::Publisher activation_right_valve_pub_;
+			ros::Publisher activation_pump_pub_;
+	};	
+
+    class ArmNode : public BT::SyncActionNode
+	{
+		public:
+			ArmNode(const std::string& name, const BT::NodeConfiguration& config);
+
+			BT::NodeStatus tick() override;
+
+		    	static BT::PortsList providedPorts()
+		    	{
+			    return { 
+				    	BT::InputPort<double>("angle_base"), 
 					BT::InputPort<double>("angle_pince"),
 				};
 			}
 		private:
 			ros::Publisher angle_base_pub_;
-        	ros::Publisher angle_pince_pub_;
+        		ros::Publisher angle_pince_pub_;
         	
 	};
 }
--- ros/src/sd_behavior/src/gripper_nodes.cpp
+++ ros/src/sd_behavior/src/gripper_nodes.cpp
@@ -1,14 +1,16 @@
 #include "sd_behavior/gripper_nodes.h"
-#include <std_msgs/UInt16.h>
+#include "std_msgs/Int16.h"
+#include "std_msgs/UInt16.h"
 
 namespace GripperNodes
 {
 	void registerNodes(BT::BehaviorTreeFactory& factory) 
 	{
+		factory.registerNodeType<GripperNodes::ArmNode>("Bras");
 		factory.registerNodeType<GripperNodes::GripperNode>("Pince");
 	}
 
-    GripperNode::GripperNode(
+    ArmNode::ArmNode(
         const std::string& name, 
         const BT::NodeConfiguration& config) 
 			: SyncActionNode(name, config)
@@ -18,7 +20,7 @@ namespace GripperNodes
         angle_pince_pub_ = nh.advertise<std_msgs::UInt16>("servo/E", 1);
     }
 
-	BT::NodeStatus GripperNode::tick()
+	BT::NodeStatus ArmNode::tick()
 	{	
         unsigned angle_base, angle_pince;
 	    if( !getInput("angle_base", angle_base) )
@@ -26,8 +28,8 @@ namespace GripperNodes
 	    if( !getInput("angle_pince", angle_pince) )
 	        throw BT::RuntimeError("linear_y is missing");
 	 
-        ROS_DEBUG_STREAM_NAMED("GripperNode", 
-			"Publish pince - " 
+        ROS_DEBUG_STREAM_NAMED("ArmNode", 
+			"Publish bras - " 
 			<< "angle base: " << angle_base
 			<< ", angle pince: " << angle_pince);
 
@@ -38,6 +40,101 @@ namespace GripperNodes
         std_msgs::UInt16 angle_pince_msg;
         angle_pince_msg.data = angle_pince;
         angle_pince_pub_.publish(angle_pince_msg);
+
+	    return BT::NodeStatus::SUCCESS;
+	}
+
+    GripperNode::GripperNode(
+        const std::string& name, 
+        const BT::NodeConfiguration& config) 
+			: SyncActionNode(name, config)
+    { 
+        ros::NodeHandle nh;
+
+        activation_right_valve_pub_ = nh.advertise<std_msgs::Int16>("pwm/vanne1", 1);
+        activation_middle_valve_pub_ = nh.advertise<std_msgs::Int16>("pwm/vanne3", 1);
+	activation_left_valve_pub_ = nh.advertise<std_msgs::Int16>("pwm/vanne2", 1);
+        activation_pump_pub_ = nh.advertise<std_msgs::Int16>("pwm/pompe", 1);
+    }
+
+	BT::NodeStatus GripperNode::tick()
+	{
+	bool left_valve, middle_valve, right_valve;
+	    if( !getInput("left_valve", left_valve) )
+	        throw BT::RuntimeError("left_valve is missing");
+	    if( !getInput("middle_valve", middle_valve) )
+	        throw BT::RuntimeError("middle_valve is missing");
+	    if( !getInput("right_valve", right_valve) )
+	        throw BT::RuntimeError("right_valve is missing");
+
+        ROS_DEBUG_STREAM_NAMED("GripperNode", 
+			"Publish pince - " 
+			<< "left valve: " << left_valve
+			<< "middle valve: " << middle_valve
+			<< "right valve: " << right_valve);
+
+	if(left_valve)
+	{
+                std_msgs::Int16 msg;
+                msg.data = 4096;
+                activation_pump_pub_.publish(msg);
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+				
+                activation_left_valve_pub_.publish(msg);
+	}
+	else
+	{
+                std_msgs::Int16 msg;
+                msg.data = 0;
+                activation_pump_pub_.publish(msg);
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+                activation_left_valve_pub_.publish(msg);
+	}
+
+	if(right_valve)
+        {
+                std_msgs::Int16 msg;
+                msg.data = 4096;
+                activation_pump_pub_.publish(msg);
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+                activation_right_valve_pub_.publish(msg);
+        }
+        else
+        {
+                std_msgs::Int16 msg;
+                msg.data = 0;
+                activation_pump_pub_.publish(msg);
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+                activation_right_valve_pub_.publish(msg);
+        }
+
+        if(middle_valve)
+        {
+                std_msgs::Int16 msg;
+                msg.data = 4096;
+                activation_pump_pub_.publish(msg);
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+                activation_middle_valve_pub_.publish(msg);
+        }
+        else
+        {
+                std_msgs::Int16 msg;
+                msg.data = 0;
+                activation_pump_pub_.publish(msg);
+
+                std::this_thread::sleep_for(std::chrono::milliseconds(10));
+
+                activation_middle_valve_pub_.publish(msg);
+        }
 
 	    return BT::NodeStatus::SUCCESS;
 	}
