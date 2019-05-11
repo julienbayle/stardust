95fc07a4ede448286a984a87be7634fdfcfb8499
Julien BAYLE <jbayle@gmail.com>
2019-05-11 13:04:14 +0200

    Improve stop with a wait loop

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -5,6 +5,13 @@ if [ "$#" -eq 0 ]; then
 	if [ -f /tmp/ros.pid ]; then
 		echo "Stopping ROS..."
 		kill -n SIGINT `cat /tmp/ros.pid`
+		sleep 1
+		ROS_COUNT=`ps -aux | grep roslaunch | wc -l`
+		while [ ! "$ROS_COUNT" -eq 1 ]; do
+			echo "ROS is still stopping... please WAIT"
+			sleep 1
+			ROS_COUNT=`ps -aux | grep roslaunch | wc -l`
+		done
 		rm /tmp/ros.pid
 		echo "ROS stopped"
 	else
