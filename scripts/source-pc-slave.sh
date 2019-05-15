abc7cec5ce85e9df09a53b0805ea81c4250ccbc4
Yann BOURRIGAULT <yann@bourrigault.fr>
2019-05-15 21:00:26 +0200

    Do not exit terminal after message

--- scripts/source-pc-slave.sh
+++ scripts/source-pc-slave.sh
@@ -2,10 +2,9 @@
 
 if [ "$#" -ne 1 ]; then
     echo "Please provide a robot hostname (after adding it into your /etc/hosts)"
-    exit 1
+else
+    export ROS_MASTER_NAME=$1
+    export ROS_MASTER_IP=`getent hosts ${ROS_MASTER_NAME} | awk '{ print $1; }'`
+    export ROS_IP=`ip route get ${ROS_MASTER_IP} | awk '{print $5; exit}'`
+    export ROS_MASTER_URI=http://${ROS_MASTER_NAME}:11311
 fi
-
-export ROS_MASTER_NAME=$1
-export ROS_MASTER_IP=`getent hosts ${ROS_MASTER_NAME} | awk '{ print $1; }'`
-export ROS_IP=`ip route get ${ROS_MASTER_IP} | awk '{print $5; exit}'`
-export ROS_MASTER_URI=http://${ROS_MASTER_NAME}:11311
